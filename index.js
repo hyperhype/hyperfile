80674987c6f822de3af45911fda10e31853517ca
Dominic Tarr <dominic.tarr@gmail.com>
2016-07-24 12:58:33 +1200

    support reading as buffer (default) or as dataurl

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 var h = require('hyperscript')
 
-module.exports = function (onFile) {
-
+function select (ready) {
   return h('input', {type: 'file', onchange: function (ev) {
     var file = ev.target.files[0]
-    var reader = new FileReader()
+    ready(new FileReader(), file)
+  }})
+
+}
+
+module.exports = function (onFile) {
+  return select(function (reader) {
     reader.onload = function () {
       onFile(new Buffer(reader.result))
     }
     reader.readAsArrayBuffer(file)
-  }})
-
+  })
 }
+
+module.exports.asDataURL = function (onFile) {
+  return select(function (reader, file) {
+    reader.onload = function () {
+      onFile(reader.result)
+    }
+    reader.readAsDataURL(file)
+  })
+}
+
+
